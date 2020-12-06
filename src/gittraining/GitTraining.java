2817e9c32499c9f71d795ce361918bf2bba935ca
Javier Martínez Ruiz <javiermaruiz@hotmail.com>
2020-12-06 07:53:52 +0100

    GitAprendiendo

---
+++ src/gittraining/GitTraining.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gittraining;
+
+/**
+ *
+ * @author javie
+ */
+public class GitTraining {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Mi código salvará al mundo...");
+        System.out.println("...eliminará el hambre...");
+        System.out.println("...atraerá a la comunidad extraterrestre...");
+        System.out.println("...bla bla bla...");
+    }
+ 
+    
+}
